2d7dd85fa852a677ae8de23ff95b76cb65729805
Leandro M. Peralta <argenkiwi@hotmail.com>
2025-07-28 21:28:23 +1200

    Tidy up python implementation.

--- python/app.py
+++ python/app.py
@@ -1,6 +1,8 @@
 import asyncio
 import random
+
 from ambler import amble
+
 
 # Nodes.
 class Node:
@@ -9,17 +11,20 @@ class Node:
     STEP = 2
     STOP = 3
 
+
 def prompt_number(state):
-    while True:
-        try:
-            num = int(input("Enter a number to start counting from: "))
-            return num, Node.START
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
+    try:
+        num = int(input("Enter a number to start counting from: "))
+        return num, Node.START
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+        return state, Node.PROMPT_NUMBER
+
 
 def start(state):
     print(f"Let's count from {state}...")
     return state, Node.STEP
+
 
 async def step(state):
     count = state + 1
@@ -27,9 +32,11 @@ async def step(state):
     print(f"...{count}...")
     return count, Node.STEP if random.choice([True, False]) else Node.STOP
 
+
 def stop(state):
     print("...stop.")
     return state, None
+
 
 # Flow.
 async def direct(state, node):
@@ -42,8 +49,10 @@ async def direct(state, node):
     elif node == Node.STOP:
         return stop(state)
 
+
 async def main():
     await amble(0, Node.PROMPT_NUMBER, direct)
 
+
 if __name__ == "__main__":
-   asyncio.run(main())
+    asyncio.run(main())
