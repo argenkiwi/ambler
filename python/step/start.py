fe8144f0fe63536d3c81d835b63fd5b3b2decd72
Leandro Peralta <leandro.peralta@gethomesafe.com>
2025-08-04 09:29:11 +1200

    Python: eliminate while loop from start.

--- python/step/start.py
+++ python/step/start.py
@@ -4,15 +4,15 @@ from lead import Lead
 
 
 async def start(state: int) -> Tuple[int, Optional[Lead]]:
-    while True:
-        user_input = input("Enter a starting number (or press Enter for default 0): ")
-        if not user_input:
-            print(f"Starting count from default: {state}")
-            return state, Lead.COUNT
-        try:
-            initial_count = int(user_input)
-            state = initial_count
-            print(f"Starting count from: {state}")
-            return state, Lead.COUNT
-        except ValueError:
-            print("Invalid input. Please enter a valid number or press Enter.")
+    user_input = input("Enter a starting number (or press Enter for default 0): ")
+    if not user_input:
+        print(f"Starting count from default: {state}")
+        return state, Lead.COUNT
+    try:
+        initial_count = int(user_input)
+        state = initial_count
+        print(f"Starting count from: {state}")
+        return state, Lead.COUNT
+    except ValueError:
+        print("Invalid input. Please, try again.")
+        return state, Lead.START
