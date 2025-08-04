fb43f54e66c429a7d34a4073100381ffd687a4ce
Leandro M. Peralta <argenkiwi@hotmail.com>
2025-08-04 21:44:16 +1200

    PHP: simplify.

--- php/ambler.php
+++ php/ambler.php
@@ -4,18 +4,6 @@ interface Step {
     public function resolve($state);
 }
 
-class NextStep implements Step {
-    private $delegate;
-
-    public function __construct(callable $delegate) {
-        $this->delegate = $delegate;
-    }
-
-    public function resolve($state) {
-        return call_user_func($this->delegate, $state);
-    }
-}
-
 function amble($state, $lead, callable $follow) {
     $currentLead = $lead;
     $currentState = $state;
